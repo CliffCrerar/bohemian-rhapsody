d8fb8848b46ea5805293ad49485ce8d47a2ac933
C1i44 <cliff@infinityarc.net>
2020-03-18 20:01:50 +0200

    generated new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppLoginComponent } from './components/app-login/app-login.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '/', component: AppLoginComponent,data:{ title: 'Login' }}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
+
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,21 @@
-import { BrowserModule} from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AppLoginComponent } from './components/app-login/app-login.component';
+import { AppLandingComponent } from './components/app-landing/app-landing.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppLoginComponent,
+    AppLandingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
